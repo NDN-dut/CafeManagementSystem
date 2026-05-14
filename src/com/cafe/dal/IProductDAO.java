e26e501cea60fa2a2f4eb487f6c72fbc8993da7f
Thinh <dangt.thinh7370@gmail.com>
2026-05-14 11:01:08 +0700

    q

--- src/com/cafe/dal/IProductDAO.java
+++ src/com/cafe/dal/IProductDAO.java
@@ -4,7 +4,7 @@ import com.cafe.model.Product;
 import com.cafe.model.Category;
 import java.util.List;
 
-// A. Quản lý Thực đơn (Thành viên 1)
+// Quản lý Thực đơn
 public interface IProductDAO extends IBaseDAO<Product, Integer> {
     List<Product> findByCategoryId(int categoryId);
     List<Product> searchByName(String name);
