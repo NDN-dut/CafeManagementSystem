8e287b46ff004cba73cde6c760b7973d30610a41
Thinh <dangt.thinh7370@gmail.com>
2026-05-14 11:01:52 +0700

    q

--- src/com/cafe/dal/ITableDAO.java
+++ src/com/cafe/dal/ITableDAO.java
@@ -4,8 +4,7 @@ import com.cafe.model.CafeTable;
 import com.cafe.model.Order;
 import com.cafe.model.OrderDetail;
 
-// B. Quản lý Bàn & Đặt món (Thành viên 2)
+// Quản lý Bàn & Đặt món
 public interface ITableDAO extends IBaseDAO<CafeTable, Integer> {
-    // Hàm đặc hiệu để đổi trạng thái bàn nhanh
     void updateStatus(int tableId, boolean isOccupied);
 }
